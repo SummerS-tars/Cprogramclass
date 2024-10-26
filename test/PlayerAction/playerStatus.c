d1027f6697aeea44b8431a8f4d700b673d641e62
TheSumSt <2240765672@qq.com>
2024-10-26 22:32:37 +0800

    little change

---
+++ test/PlayerAction/playerStatus.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+extern struct playerInfo
+{
+    int px , py ;               // 位置信息
+    int energyCost ;            // 体力消耗
+    int getTreasure ;           // 获得宝物数量
+    int getTrap ;               // 是否触发陷阱
+};
+
+extern statusJudge( struct playerInfo *player )
+{
+    
+}
+
+extern moveAction( struct playerInfo *player , int direction )
+{
+    switch( direction )
+    {
+        case 'w' : player->py ++ ; break ;
+        case 's' : player->py -- ; break ;
+        case 'a' : player->px -- ; break ;
+        case 'd' : player->px ++ ; break ;
+        default : break ;
+    }
+}
