3ceeaeb70faaaa40bd2a081d807bf6b850a4c58d
TheSumSt <2240765672@qq.com>
2024-12-28 01:21:27 +0800

    PJ2 ver2.3

--- Cprogramclass2/Project/PJ2/renew/mapLoad/04_mapCustomUpdate.h
+++ Cprogramclass2/Project/PJ2/renew/mapLoad/04_mapCustomUpdate.h
@@ -1,7 +1,7 @@
 #ifndef MAPCUSTOM_H
 #define MAPCUSTOM_H
 
-extern void mapCustomIni( int customMapOrder , int priInfo );    // ?????????¨²????
-extern void mapCustomEdit( int customMapOrder , int loadFlag);   // ???????¨²????
+extern void mapCustomIni( int customMapOrder , int priInfo );    // ??????Ë??ú?Ôš?
+extern void mapCustomEdit( int customMapOrder , int loadFlag);   // ???­Ë??ú?Ôš?
 
 #endif // MAPCUSTOM_H
---
+++ Cprogramclass2/Project/PJ2/renew/src ver2.3/src/00_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "01_menuUpdate.h"
+#include "02_cursorOperation.h"
+#include "03_mapAndPlayerStatus.h"
+#include "04_mapCustomUpdate.h"
+
+int main()
+{
+    // 进入UI实体，开始游戏
+    runMenu();
+    return 0;
+}
